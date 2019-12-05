ba58f976dc54c319c06067c98a03fe76ad5f3afe
Shaw Young <Shawson@users.noreply.github.com>
2019-12-05 18:11:05 +0000

    Adding better logging (#4)
    * update ci build to include develop branch
    
    * Adding logging and better error handling (#3)

--- OculusLibrary/OculusWebsiteScraper.cs
+++ OculusLibrary/OculusWebsiteScraper.cs
@@ -13,32 +13,49 @@ namespace OculusLibrary
     public class OculusWebsiteScraper
     {
         private readonly JavaScriptSerializer serialiser;
+        private readonly ILogger logger;
 
-        public OculusWebsiteScraper()
+        public OculusWebsiteScraper(ILogger logger)
         {            
             serialiser = new JavaScriptSerializer();
+            this.logger = logger;
         }
 
         public OculusWebsiteJson ScrapeDataForApplicationId(IWebView view, string appId)
         {
+            logger.Debug($"Trying to scrape {appId}");
+
             // robo recall 1081190428622821
-
-            view.NavigateAndWait($"https://www.oculus.com/experiences/rift/{appId}/");
-            var source = view.GetPageSource();
-
-            // get the json block from the source which contains the games meta data
-
-            Regex regex = new Regex(@"<script type=""application\/ld\+json"">([\s\S]*?)<\/script>");
-            var json = regex.Match(source);
-
-            if (json == null || json.Groups.Count < 2)
+            try
             {
+                view.NavigateAndWait($"https://www.oculus.com/experiences/rift/{appId}/");
+                var source = view.GetPageSource();
+
+                // get the json block from the source which contains the games meta data
+
+                Regex regex = new Regex(@"<script type=""application\/ld\+json"">([\s\S]*?)<\/script>");
+                var json = regex.Match(source);
+
+                if (json == null)
+                {
+                    logger.Error($"json file was null");
+                    return null;
+                }
+                if (json.Groups.Count < 2)
+                {
+                    logger.Error($"json had {json.Groups.Count} regex match groups- was expecting 2 or more");
+                    return null;
+                }
+
+                var manifest = serialiser.Deserialize<OculusWebsiteJson>(json.Groups[1].Value);
+
+                return manifest;
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exception trying to scrape {appId} : {ex}");
                 return null;
             }
-
-            var manifest = serialiser.Deserialize<OculusWebsiteJson>(json.Groups[1].Value);
-
-            return manifest;
         }
     }
 }
