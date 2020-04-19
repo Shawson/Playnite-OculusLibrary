be649f9b2428f7656d741afa9b3fe9b747196116
Shaw Young <Shawson@users.noreply.github.com>
2020-04-19 13:27:39 +0100

    Adding installation path as part of parsing process (#15)

--- OculusLibrary/OculusManifest.cs
+++ OculusLibrary/OculusManifest.cs
@@ -1,4 +1,7 @@
-﻿namespace OculusLibrary
+﻿using Newtonsoft.Json;
+using System;
+
+namespace OculusLibrary
 {
     internal class OculusManifest
     {
@@ -6,5 +9,24 @@
         public string LaunchFile { get; set; }
         public string LaunchParameters { get; set; }
         public string CanonicalName { get; set; }
+
+        public static OculusManifest Parse(string json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                throw new ArgumentException("JSON string cannot be null and empty");
+            }
+
+            var manifest = JsonConvert.DeserializeObject<OculusManifest>(json);
+
+            if (manifest == null)
+            {
+                throw new ManifestParseException("Could not deserialise json");
+            }
+
+            manifest.LaunchFile = manifest?.LaunchFile?.Replace("/", @"\");
+
+            return manifest;
+        }
     }
 }
